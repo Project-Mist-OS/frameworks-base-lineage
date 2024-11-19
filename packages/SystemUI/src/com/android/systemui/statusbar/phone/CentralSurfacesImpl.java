4adeebf6d65cd5954b08768ff2b2674c371affeb
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-19 11:39:43 +0800

    SystemUI: Introduce Depth Wallpaper feature [1/2]
    * like ios wallpaper depth, taken from pixel xpert
    
    Co-authored-by: Siavash <siavash@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -242,6 +242,7 @@ import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.util.DumpUtilsKt;
 import com.android.systemui.util.MediaArtUtils;
 import com.android.systemui.util.WallpaperController;
+import com.android.systemui.util.WallpaperDepthUtils;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.concurrency.MessageRouter;
 import com.android.systemui.util.kotlin.JavaAdapter;
@@ -446,6 +447,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final Lazy<LightRevealScrimViewModel> mLightRevealScrimViewModelLazy;
 
     private final MediaArtUtils mMediaArtUtils;
+    private final WallpaperDepthUtils mWallpaperDepthUtils;
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -878,6 +880,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             mContext.getApplicationInfo().setEnableOnBackInvokedCallback(true);
         }
         mMediaArtUtils = MediaArtUtils.getInstance(mContext);
+        mWallpaperDepthUtils = WallpaperDepthUtils.getInstance(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1155,7 +1158,15 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                 (requestTopUi, componentTag) -> mMainExecutor.execute(() ->
                         mNotificationShadeWindowController.setRequestTopUi(
                                 requestTopUi, componentTag))));
-		getNotifContainerParentView().addView(mMediaArtUtils.getMediaArtScrim(), 0);
+        ViewGroup parentView = getNotifContainerParentView();
+		parentView.addView(mMediaArtUtils.getMediaArtScrim(), 0);
+        View placeholder = parentView.findViewById(R.id.depth_wallpaper_placeholder);
+        View depthWallpaperView = mWallpaperDepthUtils.getDepthWallpaperView();
+        if (placeholder != null) {
+            int index = parentView.indexOfChild(placeholder);
+            parentView.removeView(placeholder);
+            parentView.addView(depthWallpaperView, index);
+        }
     }
     
     
@@ -2661,6 +2672,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
             mMediaArtUtils.updateMediaArtVisibility();
+            mWallpaperDepthUtils.updateDepthWallpaperVisibility();
         }
 
         @Override
