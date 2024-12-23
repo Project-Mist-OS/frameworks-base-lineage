92f9769100a018d5da55ff217ffc8f52a81b7051
Dil3mm4 <dil3mm4.dev@gmail.com>
2024-12-23 02:55:34 +0000

    core: More infos uploaded to MkrBin Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com> Change-Id: I88727f8801e99cb208fd6ff390636f61531b64e8 Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -46,6 +46,7 @@ import android.os.Bundle;
 import android.os.Message;
 import android.os.Process;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -657,9 +658,13 @@ class AppErrors {
 
 
             // Add paste content for Memochō option
+            String devfp = SystemProperties.get("ro.vendor.build.fingerprint", "");
+            String mistVers = SystemProperties.get("ro.mist.version", "");
             data.paste = "time: " + timeMillis + "\n" +
-                    "msg: " + longMsg + "\n" +
-                    "stacktrace: " + stackTrace;
+            "device fp:" + devfp + "\n" +
+            "mist vers:" + mistVers + "\n" +
+            "msg: " + longMsg + "\n" +
+            "stacktrace: " + stackTrace;
 
             final Message msg = Message.obtain();
             msg.what = ActivityManagerService.SHOW_ERROR_UI_MSG;
