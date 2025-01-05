RisingOS-staging/android_frameworks_base_new

Introduce bootanimation styles [1/3]
Squashed:

	Add support for custom bootanimation styles - Alvin Francis

	BootAnimation: Wait for boot animation directory before preloading

	   * In some scenarios, the boot animation may be preloaded before the /data/misc/bootanim directory is ready.
  	     This leads to a race condition where the default boot animation is loaded, even though custom bootanimation style is enabled.

Co-authored-by: Alvin Francis <nivlafx@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

cmds/bootanimation/BootAnimation.cpp
@@ -71,12 +71,13 @@ using ui::DisplayMode;
 
 static const char OEM_BOOTANIMATION_FILE[] = "/oem/media/bootanimation.zip";
 static const char PRODUCT_BOOTANIMATION_DARK_FILE[] = "/product/media/bootanimation-dark.zip";
-static const char PRODUCT_BOOTANIMATION_FILE[] = "/product/media/bootanimation.zip";
 static const char SYSTEM_BOOTANIMATION_FILE[] = "/system/media/bootanimation.zip";
 static const char APEX_BOOTANIMATION_FILE[] = "/apex/com.android.bootanimation/etc/bootanimation.zip";
 static const char OEM_SHUTDOWNANIMATION_FILE[] = "/oem/media/shutdownanimation.zip";
 static const char PRODUCT_SHUTDOWNANIMATION_FILE[] = "/product/media/shutdownanimation.zip";
 static const char SYSTEM_SHUTDOWNANIMATION_FILE[] = "/system/media/shutdownanimation.zip";
+static const char DEFAULT_BOOTANIMATION_FILE[] = "/product/media/bootanimation_rising.zip";
+static const char CUSTOM_BOOTANIMATION_FILE[] = "/data/misc/bootanim/bootanimation.zip";
 
 static constexpr const char* PRODUCT_USERSPACE_REBOOT_ANIMATION_FILE = "/product/media/userspace-reboot.zip";
 static constexpr const char* OEM_USERSPACE_REBOOT_ANIMATION_FILE = "/oem/media/userspace-reboot.zip";
@@ -226,11 +227,38 @@ BootAnimation::~BootAnimation() {
             elapsedRealtime());
 }
 
+bool isDirectoryAccessible(const char* path) {
+    struct stat info;
+    return (stat(path, &info) == 0 && (info.st_mode & S_IFDIR));
+}
+
+void waitForBootAnimDir() {
+    int64_t waitStartTime = elapsedRealtime();
+    const char* bootAnimPath = "/data/misc/bootanim/";
+    const int DIR_WAIT_SLEEP_MS = 100;
+    const int LOG_PER_RETRIES = 10;
+    int retry = 0;
+    while (!isDirectoryAccessible(bootAnimPath)) {
+        retry++;
+        if ((retry % LOG_PER_RETRIES) == 0) {
+            ALOGD("Waiting for %s to be accessible, waited for %" PRId64 " ms",
+                  bootAnimPath, elapsedRealtime() - waitStartTime);
+        }
+        usleep(DIR_WAIT_SLEEP_MS * 1000);
+    }
+
+    int64_t totalWaited = elapsedRealtime() - waitStartTime;
+    if (totalWaited > DIR_WAIT_SLEEP_MS) {
+        ALOGD("Waiting for %s took %" PRId64 " ms", bootAnimPath, totalWaited);
+    }
+}
+
 void BootAnimation::onFirstRef() {
     ATRACE_CALL();
     status_t err = mSession->linkToComposerDeath(this);
     SLOGE_IF(err, "linkToComposerDeath failed (%s) ", strerror(-err));
     if (err == NO_ERROR) {
+        waitForBootAnimDir();
         // Load the animation content -- this can be slow (eg 200ms)
         // called before waitForSurfaceFlinger() in main() to avoid wait
         ALOGD("%sAnimationPreloadTiming start time: %" PRId64 "ms",
@@ -765,10 +793,8 @@ bool BootAnimation::findBootAnimationFileInternal(const std::vector<std::string>
 
 void BootAnimation::findBootAnimationFile() {
     ATRACE_CALL();
-    const bool playDarkAnim = android::base::GetIntProperty("ro.boot.theme", 0) == 1;
     static const std::vector<std::string> bootFiles = {
-        APEX_BOOTANIMATION_FILE, playDarkAnim ? PRODUCT_BOOTANIMATION_DARK_FILE : PRODUCT_BOOTANIMATION_FILE,
-        OEM_BOOTANIMATION_FILE, SYSTEM_BOOTANIMATION_FILE
+        CUSTOM_BOOTANIMATION_FILE, DEFAULT_BOOTANIMATION_FILE
     };
     static const std::vector<std::string> shutdownFiles = {
         PRODUCT_SHUTDOWNANIMATION_FILE, OEM_SHUTDOWNANIMATION_FILE, SYSTEM_SHUTDOWNANIMATION_FILE, ""

Project-Mist-OS/frameworks-base-lineage

cmds/bootanimation/BootAnimation.cpp
@@ -70,14 +70,14 @@ namespace android {
 using ui::DisplayMode;
 
 static const char OEM_BOOTANIMATION_FILE[] = "/oem/media/bootanimation.zip";
-static const char PRODUCT_BOOTANIMATION_DIR[] = "/product/media/";
-static const char PRODUCT_BOOTANIMATION_DARK_FILE[] = "bootanimation-dark.zip";
-static const char PRODUCT_BOOTANIMATION_FILE[] = "bootanimation.zip";
+static const char PRODUCT_BOOTANIMATION_DARK_FILE[] = "/product/media/bootanimation-dark.zip";
 static const char SYSTEM_BOOTANIMATION_FILE[] = "/system/media/bootanimation.zip";
 static const char APEX_BOOTANIMATION_FILE[] = "/apex/com.android.bootanimation/etc/bootanimation.zip";
 static const char OEM_SHUTDOWNANIMATION_FILE[] = "/oem/media/shutdownanimation.zip";
 static const char PRODUCT_SHUTDOWNANIMATION_FILE[] = "/product/media/shutdownanimation.zip";
 static const char SYSTEM_SHUTDOWNANIMATION_FILE[] = "/system/media/shutdownanimation.zip";
+static const char DEFAULT_BOOTANIMATION_FILE[] = "/product/media/bootanimation_mist.zip";
+static const char CUSTOM_BOOTANIMATION_FILE[] = "/data/misc/bootanim/bootanimation.zip";
 
 static constexpr const char* PRODUCT_USERSPACE_REBOOT_ANIMATION_FILE = "/product/media/userspace-reboot.zip";
 static constexpr const char* OEM_USERSPACE_REBOOT_ANIMATION_FILE = "/oem/media/userspace-reboot.zip";
@@ -227,11 +227,38 @@ BootAnimation::~BootAnimation() {
             elapsedRealtime());
 }
 
+bool isDirectoryAccessible(const char* path) {
+    struct stat info;
+    return (stat(path, &info) == 0 && (info.st_mode & S_IFDIR));
+}
+
+void waitForBootAnimDir() {
+    int64_t waitStartTime = elapsedRealtime();
+    const char* bootAnimPath = "/data/misc/bootanim/";
+    const int DIR_WAIT_SLEEP_MS = 100;
+    const int LOG_PER_RETRIES = 10;
+    int retry = 0;
+    while (!isDirectoryAccessible(bootAnimPath)) {
+        retry++;
+        if ((retry % LOG_PER_RETRIES) == 0) {
+            ALOGD("Waiting for %s to be accessible, waited for %" PRId64 " ms",
+                  bootAnimPath, elapsedRealtime() - waitStartTime);
+        }
+        usleep(DIR_WAIT_SLEEP_MS * 1000);
+    }
+
+    int64_t totalWaited = elapsedRealtime() - waitStartTime;
+    if (totalWaited > DIR_WAIT_SLEEP_MS) {
+        ALOGD("Waiting for %s took %" PRId64 " ms", bootAnimPath, totalWaited);
+    }
+}
+
 void BootAnimation::onFirstRef() {
     ATRACE_CALL();
     status_t err = mSession->linkToComposerDeath(this);
     SLOGE_IF(err, "linkToComposerDeath failed (%s) ", strerror(-err));
     if (err == NO_ERROR) {
+        waitForBootAnimDir();
         // Load the animation content -- this can be slow (eg 200ms)
         // called before waitForSurfaceFlinger() in main() to avoid wait
         ALOGD("%sAnimationPreloadTiming start time: %" PRId64 "ms",
@@ -766,13 +793,8 @@ bool BootAnimation::findBootAnimationFileInternal(const std::vector<std::string>
 
 void BootAnimation::findBootAnimationFile() {
     ATRACE_CALL();
-    const bool playDarkAnim = android::base::GetIntProperty("ro.boot.theme", 0) == 1;
-    const std::string productBootanimationFile = PRODUCT_BOOTANIMATION_DIR +
-        android::base::GetProperty("ro.product.bootanim.file", playDarkAnim ?
-        PRODUCT_BOOTANIMATION_DARK_FILE : PRODUCT_BOOTANIMATION_FILE);
     static const std::vector<std::string> bootFiles = {
-        APEX_BOOTANIMATION_FILE, productBootanimationFile,
-        OEM_BOOTANIMATION_FILE, SYSTEM_BOOTANIMATION_FILE
+        CUSTOM_BOOTANIMATION_FILE, DEFAULT_BOOTANIMATION_FILE
     };
     static const std::vector<std::string> shutdownFiles = {
         PRODUCT_SHUTDOWNANIMATION_FILE, OEM_SHUTDOWNANIMATION_FILE, SYSTEM_SHUTDOWNANIMATION_FILE, ""